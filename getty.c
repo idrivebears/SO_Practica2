a638891b8bd318adbe870a9d9eddbf4dc9e8a9d3
idrivebears <idrivebears@gmail.com>
2017-02-04 12:21:20 -0600

    getty init

--- getty.c
+++ getty.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/time.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-	
+	char * f_name = "password.txt";
+
+	// Recibe seg_id del proceso ini
+	if (argc < 2) {
+		return 1;
+	}
+
+	int seg_int = atoi(argv[1]);
+	printf("Segment ID received: %d\n", seg_int);
+	while(1) {
+		return 0;
+	}
 }
