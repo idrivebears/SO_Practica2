d7ec5d06dd3fa0b612120a61bf9cbf1f1abd89e0
idrivebears <idrivebears@gmail.com>
2017-02-04 12:06:56 -0600

    process

--- init.c
+++ init.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/time.h>
 
-int main(int argc, char *argv[])
+#define NPROCS 4
+
+int proceso(int i)
 {
-	
+    sleep(1);
+    printf("Proceso %d con pid %d\n",i, getpid());
+    exit(0);
+}
+
+int main()
+{
+    int i;
+    int pid;
+    int status;
+
+    for(i = 0;i < NPROCS;i++) {
+        pid=fork();
+
+        if(pid==0) {
+        	proceso(i);
+        }
+    }
+
+    for(i=0;i<NPROCS;i++) {
+        wait(&status);
+    }
+    
+    printf("Fin\n");
 }
