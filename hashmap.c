86aa249a647118be0c4e584fc5733270b3b33f21
AlexW <idrivebears@gmail.com>
2017-02-06 18:02:23 -0600

    useless hashmap

--- hashmap.c
+++ hashmap.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-int hash(char * input, int range) 
+unsigned int hash(char * input, int range) 
 {
 	int i;
 	int n = (int)strlen(input);
-	int res = 0;
+	int res = 5381;
 
 	for(i=0; i < n; i++) {
-		res += (int)input[i] * i;
+		res = ((res << 5) + res) + input[i];
 	}
 
 	return res % range;
@@ -17,7 +17,7 @@ int hash(char * input, int range)
 
 int *create_hashmap(int size) 
 {
-	int map[size];
+	int *map = (int *) malloc(sizeof(int)*size);
 	int i;
 	
 	for(i = 0; i < size; i++) {
@@ -35,7 +35,9 @@ int get_hashmap(int *map, int i)
 int insert_hashmap(int *map, char * input, int value)
 {
 	int range = sizeof(map)/sizeof(int);
-	int location = hash(input, range);
+	unsigned int location = hash(input, range);
+
+	printf("Inserting val:%d into loc:%d\n", value,location);
 
 	if(map[location] == -1) {
 		map[location] = value;
@@ -44,4 +46,33 @@ int insert_hashmap(int *map, char * input, int value)
 	else {
 		return 0; // fail, collision found
 	}
+}
+
+int main()
+{
+	int MAP_SIZE = 20;
+	int *my_map = create_hashmap(MAP_SIZE);
+
+	int i;
+	for(i = 0; i < MAP_SIZE; i++) 
+	{
+		printf("%d,", my_map[i]);
+	}
+	printf("\n");
+
+	insert_hashmap(my_map, "thisisatest",hash("birds", MAP_SIZE));
+	
+	if(insert_hashmap(my_map, "thisisbtest", hash("birds", MAP_SIZE)) == 0) {
+		printf("insert failure\n");
+	}
+
+	for(i = 0; i < MAP_SIZE; i++) 
+	{
+		printf("%d,", my_map[i]);
+	}
+	printf("\n");
+
+	printf("Retrieving 'user': %d\n", get_hashmap(my_map, hash("user", MAP_SIZE)));
+
+	return 0;
 }
